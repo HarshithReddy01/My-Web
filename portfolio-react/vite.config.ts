1281e25a1c13061833a37b6b7daaa0524e4552e6
Harshith Reddy <Harshithreddy0115@gmail.com>
2025-08-12 10:38:39 -0500

    for deployment

--- portfolio-react/vite.config.ts
+++ portfolio-react/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       },
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-react/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/My-Web/' : '/',
   define: {
     'process.env': {},
     global: 'globalThis',
